b89f06406d9dace98a5f460c6fe9c92e6cbd46f4
Jan Christoph Ebersbach <jan-christoph.ebersbach@chainstep.com>
2020-03-06 16:11:16 +0100

    Update mermaid

--- wrapper.js
+++ wrapper.js
@@ -4,66 +4,61 @@ type: application/javascript
 module-type: widget
 
 \*/
-(function(){
+(function() {
+  /* jslint node: true, browser: true */
+  /* global $tw: false */
+  'use strict';
+  let uniqueID = 1;
 
-/*jslint node: true, browser: true */
-/*global $tw: false */
-"use strict";
-     var uniqueID = 1;
+  let Widget = require("$:/core/modules/widgets/widget.js").widget;
+  let Rocklib = require("$:/plugins/gt6796c/rocklib/widget-tools.js").rocklib;
+  if ($tw.browser && !window.mermaidAPI) {
+    window.rocklib = new Rocklib();
+    window.mermaidAPI = require("$:/plugins/gt6796c/mermaid-tw5/mermaid.min.js").mermaidAPI;
+  }
 
-    var Widget = require("$:/core/modules/widgets/widget.js").widget;
-    var Rocklib = require("$:/plugins/gt6796c/rocklib/widget-tools.js").rocklib;
-    if ($tw.browser && !window.mermaidAPI) {
-        window.rocklib = new Rocklib();
-        require("$:/plugins/gt6796c/mermaid-tw5/mermaidAPI.js").mermaidAPI;
-    }
+  let MermaidWidget = function(parseTreeNode, options) {
+    this.initialise(parseTreeNode, options);
+  };
 
+  MermaidWidget.prototype = new Widget();
 
-    var MermaidWidget = function(parseTreeNode, options) {
-        this.initialise(parseTreeNode, options);
-    };
-
-    MermaidWidget.prototype = new Widget();
-
-    /*
+  /*
      Render this widget into the DOM
      */
-    MermaidWidget.prototype.render = function(parent,nextSibling) {
-        this.parentDomNode = parent;
-        this.computeAttributes();
-        this.execute();
+  MermaidWidget.prototype.render = function(parent, nextSibling) {
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
 
-        var tag = 'mermaid';
-        var scriptBody = rocklib.getScriptBody(this,"text");
-        var divNode = rocklib.getCanvas(this,tag);
-        var _insertSVG = function(svgCode, bindFunctions) {
-            divNode.innerHTML = svgCode;
-        }
-        try {
-            var options = {'theme' : 'simple'};
-            rocklib.getOptions(this, tag, options);
-            mermaidAPI.render(divNode.id, scriptBody, _insertSVG);
-        }
-        catch(ex)
-        {
-            divNode.innerText = ex;
-        }
-        parent.insertBefore(divNode, nextSibling);
-
-        this.domNodes.push(divNode);
+    let tag = "mermaid";
+    let scriptBody = rocklib.getScriptBody(this, "text");
+    let divNode = rocklib.getCanvas(this, tag);
+    let _insertSVG = function(svgCode, bindFunctions) {
+      divNode.innerHTML = svgCode;
     };
+    try {
+      let options = { theme: "simple" };
+      rocklib.getOptions(this, tag, options);
+      window.mermaidAPI.render(divNode.id, scriptBody, _insertSVG);
+    } catch (ex) {
+      divNode.innerText = ex;
+    }
+    parent.insertBefore(divNode, nextSibling);
 
-    MermaidWidget.prototype.execute = function() {
-        // Nothing to do
-    };
+    this.domNodes.push(divNode);
+  };
 
-    /*
+  MermaidWidget.prototype.execute = function() {
+    // Nothing to do
+  };
+
+  /*
      Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
      */
-    MermaidWidget.prototype.refresh = function(changedTiddlers) {
-        return false;
-    };
+  MermaidWidget.prototype.refresh = function(changedTiddlers) {
+    return false;
+  };
 
-    exports.mermaid = MermaidWidget;
-
+  exports.mermaid = MermaidWidget;
 })();
